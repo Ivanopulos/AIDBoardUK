33269538bafc0636da59f1946c34112404c5fa1e
Ivanopulos <Pochta.rus.lc@gmail.com>
2022-11-25 18:34:08 +0300

    Первоя волна распознавания

--- AIDB.py
+++ AIDB.py
@@ -1,33 +1,17 @@
-# d_ папка и файлы детализации
-# s_ словари
+# d_ папка и файлы детализации d_df d_path
+# s_ словари s_uk/s_uk1 s_ddf/s_adf s_adf1
+# s_ddf словарь управляшек <01/мм*3/гггг> need to renew
+# c_ результаты сведения
+# путь по файлам:
+#   запрос в одой папке входящих, d_df.pkl/xlsx, chk.xlsx, chk1.xlsx
+import time
 import numpy as np
-import pandas as pd
+import pandas as pd #pip install xlsxwriter
 import os
 from tkinter import filedialog
-import traceback
 import regex as re
 import warnings
-warnings.simplefilter("ignore")  # dftmp = pd.read_excel(d_path+filename)
-def p():
-    #print(traceback.extract_stack())  # Полная информация о стеке вызовов
-    ns=traceback.extract_stack()[-2].lineno  # Только номер строки, откуда была вызвана функция
-    s=0
-    f = open('AIDB.py', 'r')
-    line = f.readline()
-    while line:
-        #print (line),
-        line = f.readline()
-        s = s+1
-        if s == ns-2:
-            stroka = line
-    f.close()
-    stroka=re.search(r'\S*\s', stroka)[0]
-    stroka=stroka[:-1]
-    exec("print("+stroka+")")
-    #sss = "sss1"
-    #locals()[sss] = 5000  # globals()[sss] = 5000
-    #print(sss1)
-    #print(eval("2+2"))
+#warnings.simplefilter("ignore")  # dftmp = pd.read_excel(d_path+filename)
 def toxls12():  # соединение в один дф
     d_df = pd.DataFrame()                         # дф для сборки файлов
     d_path = filedialog.askopenfilename()         # произвольный файл из папки с базой(только целевые должны быть), а база едс на шаг выше
@@ -38,10 +22,6 @@ def toxls12():  # соединение в один дф
             dftmp = pd.read_excel(d_path+filename)
             print(dftmp)
             d_df = pd.concat([d_df, dftmp], axis=0)
-    d_df.to_excel('./d_df.xlsx')                  # save
-    print('xls out')
-    d_df.to_pickle("./d_df.pkl ")
-    print('pkl out')
     return d_df
 def och(d_df):                                            # очистка го и ук
     d_df = pd.read_pickle("./d_df.pkl ")
@@ -57,40 +37,93 @@ def och(d_df):                                            # очистка го 
     d_df = d_df[d_df.Статус.isin(s_uk1)]
     #d_df = d_df[~d_df.Описание.str.startswith('Номер Добродела')]
     return d_df
-if os.path.isfile("./chk.xlsx"):                                                                      # если уже очищено
-    d_df = pd.read_excel("./chk.xlsx")
-    print(1)
-else:
-    if not os.path.isfile("./d_df.pkl"):                                                # если ничего вообще не делалось
-        d_df = toxls12()
-        print(2)
-    else:
-        d_df = pd.read_pickle("./d_df.pkl ")                                          # если уже объединено но не чищено
-        print(3)
-    d_df = och(d_df)                                                                                                    # очистка
-#kdf=d_df
-d_df["корд"] = d_df['Описание'].str.extract(r"(5[456][.,]\d{3,8}\D{1,10}3[678][.,]\d{3,8})+")                           # формирование столбцов с координатами
-d_df["Описание"] = d_df['Описание'].str.replace(r"5[456][.,]\d{2,8}\D{1,10}3[678][.,]\d{2,8}", "", regex=True)
-d_df['Описание'] = d_df['Описание'].str.replace(r"\d{5,}", "", regex=True)                                              # стирает более чем 3знач числа
-d_df['Описание'] = d_df['Описание'].str.replace(r"([0123][0123456789]\D{1,2}[01][0123456789]\D{1,2}20{,1}2{,1}2)+", "", regex=True)  # чистим даты из текста
-d_df = d_df.loc[(d_df['Описание'].str.len() > 40)|(d_df['корд'].str.len() > 2)]                                         # строка длинее 40
-d_df['Описание'] = d_df['Описание'].str.replace(r"Вопрос 1", "zzz", regex=True)                                         # заменяем цифру от фраззы чтоб не мешала удалять по признаку цифры
-d_df = d_df[~d_df['Описание'].str.contains(r"zzz\D{,2}1{,1}.{,1}$", regex=True)]                                        # Вопрос 1 в конце как признак битости строки
-d_df['Описание'] = d_df['Описание'].str.replace(r"202\d", "", regex=True)#7
-d_df['Описание'] = d_df['Описание'].str.replace(r"\d{,9}[,.]{,1}\d{1,9}\D{,2}рубл[яе]", "", regex=True)
-d_df['Описание'] = d_df['Описание'].str.replace(r"\d{,9}[,.]{,1}\d{1,9}\D{,2}копе[йие]", "", regex=True)#1!!!
-d_df['Описание'] = d_df['Описание'].str.replace(r"\d{,2}-{,1}й{,1} {,1}под[ьъ]езда{,1} {,1}\d{,2}", "", regex=True) #10
-d_df['Описание'] = d_df['Описание'].str.replace(r"\d{1,3} ?%", "", regex=True)
-d_df['Описание'] = d_df['Описание'].str.replace(r"\d{1,3} ?суто?ки?", "", regex=True)
-d_df['Описание'] = d_df['Описание'].str.replace(r"\d{1,3} ?этаже?", "", regex=True)
+def chk1(d_df):
+    d_df["корд"] = d_df['Описание'].str.extract(r"(5[456][\.,]\d{4,8}\D{1,10}3[678][\.,]\d{4,8})+")  # формирование столбцов с координатами
+    d_df["корд"] = d_df["корд"].str.replace(r"(\d\d)[\.,](\d{4,8})\D{1,10}(3[678])[\.,](\d{4,8})", r"[\1.\2,\3.\4]", regex=True)  # стандарт написания
+    d_df["Описание"] = d_df['Описание'].str.replace(r"5[456][\.,]\d{2,8}\D{1,10}3[678][\.,]\d{2,8}", "", regex=True)  # удаление координат
+    d_df['Описание'] = d_df['Описание'].str.replace(r'[\t\r\n]', "", regex=True)  # дублирует '[^0-9а-яА-Я \.,/ёЁ]' мож и не надо уже
+    # d_df['Описание'] = d_df['Описание'].str.replace(r'\-', "/", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r'[^0-9а-яА-Я \.,/ёЁ\\№\-]', "", regex=True)  # а-яА-Я ёЁйЙ.,\-
+    d_df['Описание'] = d_df['Описание'].str.replace(r'\d{2,}[/\.\-]\d{2,}[/\. \-]\d{2,}', "", regex=True)  # всякие номера типа ххх-хх-хх не захв 8 (498) 000-00-00
+    d_df['Описание'] = d_df['Описание'].str.replace(r'[87]? ?\(\d{3,}\)', "", regex=True)  # остатки ном тел но походу ничего не дает
+    d_df['Описание'] = d_df['Описание'].str.replace(r'\d{4,}', "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r'\d+ раз[ \.,]', "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r'\d?[,\.\-]?\d+ месяц', "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"\d\d[\.\-:]00", "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"\dх", "", regex=True)
+    d_df = d_df.loc[(d_df['Описание'].str.len() > 40) | (d_df['корд'].str.len() > 2)]  # строка длинее 40
+    d_df['Описание'] = d_df['Описание'].str.replace(r"Вопрос 1", "zzz", regex=True)  # заменяем цифру от фраззы чтоб не мешала удалять по признаку цифры
+    d_df = d_df[~d_df['Описание'].str.contains(r"zzz\D{,2}1{,1}\.{,1}$", regex=True)]  # Вопрос 1 в конце как признак битости строки
+    d_df['Описание'] = d_df['Описание'].str.replace(r"\d{,9}[,\.]{,1}\d{1,9}\D{,2}рубл[яе][й ,\.]", "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"\d{,9}[,\.]{,1}\d{1,9}\D{,2}копе[йие]", "", regex=True)  # 1!!!
+    d_df['Описание'] = d_df['Описание'].str.replace(r"(\d)[еягй]?\-?[еягй]?о?", r"\1", regex=True)  # 9-й в 9
+    d_df['Описание'] = d_df['Описание'].str.replace(r" \d{,2}\-{,1}й{,1}г?о? {,1}[пП]од[ьъ]езда{,1}е{,1}о?м? {,1}\d{,2}", "", regex=True)  # 10
+    d_df['Описание'] = d_df['Описание'].str.replace(r"\d{1,3} ?%", "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"\d{1,3} ?суто?ки?", "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"\d{1,3} ?[эЭ]таже?", "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"[012]?[0-9]:[012]?[0-9]", "", regex=True)  # time6968614-1
+    d_df['Описание'] = d_df['Описание'].str.replace(r"[эЭ]таж ?\d{1,3}", "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"[оО]ценка \d", "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"[рР]ассмотрение \d", "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"[вВ]опрос \d", "", regex=True)
+    d_df['Описание'] = d_df['Описание'].str.replace(r"\d[хм]", "", regex=True)
+    d_df = d_df[d_df['Описание'].str.contains(r'\d{1,}', regex=True) | (d_df['корд'].str.len() > 2)]  # есть хотябы одна цифра
+    d_df = d_df.drop_duplicates(subset=['Номер ЕЦУР'])  # 50 если описание то 100
+    print(d_df['Описание'])
+    d_df.to_excel('./chk1.xlsx')
+    return d_df
+def rasp(x):
+    global s_adf
+    global t
+    s_adf1 = s_adf[(s_adf.ОМСУ == x["ОМСУ"])&(s_adf.УК == x["Исполнитель"])]
+    s_adf1['есхожд'] = 0
+    s_adf1['улица'] = s_adf1['улица'].str.replace(r'.+, ', "", regex=True)
+    #print(s_adf1['ул'])
+    u = s_adf1.columns.get_loc("улица")
+    d = s_adf1.columns.get_loc('дом')
+    e = s_adf1.columns.get_loc('есхожд')
+    a = x["Описание"]
+    for i in range(1, len(s_adf1)):
+        b = s_adf1.iloc[i, u]
+        c = s_adf1.iloc[i, d]
+        if a.find(b) > 1 and a.find(c) > 1: # and (x["Описание"].find(str(s_adf1.iloc[i]["дом"]))>1
+            s_adf1.iloc[i, e] = 1
+            #print(s_adf1.iloc[i, e])
+    #tmp=s_adf1['есхожд'].sum()
+    #print(tmp)
+    t=t+1
+    if int(t/100)==t/100:
+        print(t)
+    if s_adf1['есхожд'].sum() == 1:
+        itg = s_adf1[s_adf1["есхожд"] == 1]["Адрес в образце"]
+        return itg
+if not os.path.isfile("./d_df.pkl"):
+    d_df = toxls12()  # сбор
+    d_df.to_excel('./d_df.xlsx')
+    print('xls out')
+    d_df.to_pickle("./d_df.pkl ")
+    print('pkl out')
+    d_df = och(d_df)  # фильтр
+    d_df.to_excel('./chk.xlsx', engine='xlsxwriter')
+    print('chk out')
+if not os.path.isfile("./chk1.xlsx"):
+    d_df = pd.read_excel("./chk.xlsx")  # очистка от битых
+    d_df = chk1(d_df)
+    print('chk1 out')
+s_adf = pd.read_excel("./адрес стандарт.xlsx")
+s_adf["ОМСУ"] = s_adf["ОМСУ"].str.replace(r' г о', "", regex=True)
+try:
+    if len(d_df) < 10:
+        print("low len")
+except:
+    d_df = pd.read_excel("./chk1.xlsx")
+    print("chk1 eated")
+t = 0
+d_df["распознано"] = d_df.apply(rasp, axis=1)
+d_df.to_excel("./chk2.xlsx")
+print(d_df["распознано"])
 
-d_df['Описание'] = d_df['Описание'].str.replace(r"\d[еягй]?-?[еягй]?о?", "\d", regex=True)
-
-d_df = d_df[d_df['Описание'].str.contains(r'\d{1,}', regex=True)|(d_df['корд'].str.len() > 2)]                          # есть хотябы одна цифра
-
-
-print(d_df['Описание'])
-d_df.to_excel('./chk1.xlsx')
-#df.isin({'Исполнитель': [0, 3]})
-
-
+#pd.options.display.max_colwidth = 1000
+#print(str(d_df[d_df["Номер ЕЦУР"] == "7500157-2"]["Описание"]).encode("UTF-8"))7258977-1
+#print(d_df[d_df["Номер ЕЦУР"] == "7258977-1"]["Описание"])
+#print(b'\xb2\xd0\xb0 -5910223 \xd0\x9e\xd1'.decode("utf-8", "ignore"))
